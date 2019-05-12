14fd2d03b4c76e025bef3df53539dc96d928d1e8
Ravi Chaudhary <ravi7131@yahoo.com>
2019-05-12 00:22:20 -0400

    Test gpg keys again

--- src/main/java/ca/ravichaudhary/apishoppingcart/ApiShoppingCartApplication.java
+++ src/main/java/ca/ravichaudhary/apishoppingcart/ApiShoppingCartApplication.java
@@ -12,11 +12,12 @@ public class ApiShoppingCartApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(ApiShoppingCartApplication.class, args);
 
-		// Insert sample dataset
+		// Insert sample product dataset
 		ProductService productService = applicationContext.getBean(ProductService.class);
 		productService.deleteAll();
 		productService.insertProductSampleData();
 
+		// Insert sample user dataset
 		UserService userService = applicationContext.getBean(UserService.class);
 		userService.deleteAll();
 		userService.insertUserSampleData();

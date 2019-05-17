7d1273277c2399b4b43ca9391343deb5f244617c
Ravi Chaudhary <ravi7131@yahoo.com>
2019-05-16 22:59:54 -0400

    Added integration test using embedded mongodb

--- src/test/java/ca/ravichaudhary/apishoppingcart/config/TestMongoConfig.java
+++ src/test/java/ca/ravichaudhary/apishoppingcart/config/TestMongoConfig.java
@@ -9,22 +9,38 @@ import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.io.IOException;
 
-//@Configuration
-public class TestMongoConfig {
+@TestConfiguration
+public class TestMongoConfig extends AbstractMongoConfiguration {
 
     private static String hostname = "localhost";
     private static int port = 27000;
     private static String databaseName = "test";
 
     @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(new MongoClient(hostname, port), databaseName);
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, getDatabaseName());
+    }
+
+    @Bean
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        return new MongoClient(hostname, port);
     }
 
     @Bean(destroyMethod = "stop")
--- src/test/java/ca/ravichaudhary/apishoppingcart/product/ProductServiceTest.java
+++ src/test/java/ca/ravichaudhary/apishoppingcart/product/ProductServiceTest.java
@@ -1,7 +1,6 @@
 package ca.ravichaudhary.apishoppingcart.product;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
